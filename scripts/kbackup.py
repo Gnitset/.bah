2016bada2ea31de2a1eea3a3a87816f583d58b8c
Klas Meder Boqvist <github.com@servern.info>
2009-03-04 19:41:52 +0000

    la till backupprogrammet

---
+++ scripts/kbackup.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python2.5
+
+import os, sys, time
+
+root="/a/backup/kbackup"
+lockfile=".kbackup.lock"
+sources={}
+
+sources["doija.int.ladan.se"]={ "interval": "d", "keep": 30, "user": "backup", "password": "ChangeMe" }
+sources["mail.oijk.net"]={ "interval": "h", "keep": 30, "user": "backup", "password": "ChangeMe" }
+
+def main():
+	try:
+		os.stat(os.path.join(root,lockfile))
+		print "lockfile exists"
+		sys.exit(1)
+	except: pass
+
+	for host in sources:
+		kb=KBackup(root, host, sources[host])
+		kb.rotate()
+		kb.sync()
+		print host, "completed"
+
+class KBackup():
+	root=""
+	host=""
+	keep="7"
+	sourcename="backup"
+	user="backup"
+	password=""
+
+	def __init__(self, root, host, conf):
+		self.root=root
+		self.host=host
+		self.keep=conf["keep"]
+		try:
+			self.user=conf["user"]
+		except: pass
+		try:
+			self.password=conf["password"]
+		except: pass
+		try:
+			self.sourcename=conf["sourcename"]
+		except: pass
+
+	def rotate(self):
+		print os.readlink(os.path.join(self.root, self.host, "current"))
+		return False
+
+	def sync(self):
+		new=os.path.join(self.root, self.host, "new")
+		current=os.path.join(self.root, self.host, "current")
+
+		print "running rsync on host", self.host, "with sourcename", self.sourcename
+		ret_val=os.spawnvpe(os.P_WAIT, "rsync", ["rsync", "--archive", "--delete", "--numeric-ids", "--hard-links", "--sparse", "--link-dest="+current, self.host+"::"+self.sourcename+"/", new], { "USER": self.user, "RSYNC_PASSWORD": self.password } )
+
+		if ret_val < 0:
+			print "cleanup"
+			os.spawnvp(os.P_WAIT, "rm", ["rm", "-r", new])
+			sys.exit(2)
+
+		stime=time.time()
+		os.utime(new), (stime, stime))
+		os.rename(new), os.path.join(self.root, self.host, time.asctime(time.localtime(stime))))
+		try:
+			os.remove(current)
+		except: pass
+		os.symlink(time.asctime(time.localtime(stime)), current)
+
+
+if __name__ == "__main__":
+	main()
