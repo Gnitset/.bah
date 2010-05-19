e470ce09ed73a2956c09a8e92fd56ff126323f8a
Klas Meder Boqvist <github.com@servern.info>
2010-05-19 16:53:46 +0000

    modification of rcptto_check after consultation from yann

--- scripts/rcptto_check.py
+++ scripts/rcptto_check.py
@@ -4,29 +4,23 @@ import os
 import sys
 import sqlite3
 
-db_file="/mail/db/rcptto.db"
-email=os.environ['SMTPRCPTTO']
+db_file = "/mail/db/rcptto.db"
+email = os.environ['SMTPRCPTTO']
 
 conn = sqlite3.connect(db_file)
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS mail (address TEXT PRIMARY KEY, occurrences INTEGER DEFAULT 1, status INTEGER DEFAULT 0)")
+c.execute("CREATE TABLE IF NOT EXISTS mail (address TEXT PRIMARY KEY, occurrences INTEGER DEFAULT 0, status INTEGER DEFAULT 0)")
 conn.commit()
 
-matches=c.execute("SELECT COUNT(*) FROM mail WHERE address=?", (email,)).fetchall()[0][0]
-
-if matches:
-	res=c.execute("UPDATE mail SET occurrences=occurrences+1 WHERE address=?", (email,))
-	conn.commit()
-	status=c.execute("SELECT status FROM mail WHERE address=?", (email,)).fetchall()[0][0]
-	if status==-1:
-		sys.stderr.write("rcptto: rejected mail to <%s>\n"%email)
-		sys.stdout.write("R550 Blocked email address <%s>.\n"%email)
-	elif status==1:
-		sys.stderr.write("rcptto: accepted mail to <%s>\n"%email)
-		sys.stdout.write("N\n")
-	else:
-		sys.stderr.write("rcptto: let mail to <%s> through\n"%email)
+res = c.execute("INSERT OR IGNORE INTO mail (address) VALUES (?)", (email,))
+res = c.execute("UPDATE mail SET occurrences=occurrences+1 WHERE address=?", (email,))
+conn.commit()
+status = c.execute("SELECT status FROM mail WHERE address=?", (email,)).fetchall()[0][0]
+if status == -1:
+	sys.stderr.write("rcptto: rejected mail to <%s>\n"%email)
+	sys.stdout.write("R550 Blocked email address <%s>.\n"%email)
+elif status == 1:
+	sys.stderr.write("rcptto: accepted mail to <%s>\n"%email)
+	sys.stdout.write("N\n")
 else:
-	res=c.execute("INSERT INTO mail (address) VALUES (?)", (email,))
-	conn.commit()
 	sys.stderr.write("rcptto: let mail to <%s> through\n"%email)
