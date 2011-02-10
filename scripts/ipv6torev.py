32791f206d0884230568e722ff3d1e4ef44120f6
Klas Meder Boqvist <github.com@servern.info>
2011-02-10 09:57:57 +0000

    added ipv6torev.py

---
+++ scripts/ipv6torev.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from sys import argv
+
+for ip in argv[1:]:
+	groups=ip.split(":")
+	missing0=6-len(groups)
+	ipv6=[]
+
+	for group in groups:
+		if len(group) == 0:
+			group="0000"+"0000"*missing0
+		ipv6=ipv6+[Z for Z in group.zfill(4)]
+
+	ipv6.reverse()
+
+	print "%s.ip6.arpa"%(".".join(ipv6))
